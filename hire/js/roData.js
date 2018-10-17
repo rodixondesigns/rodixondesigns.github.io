3cd38d5f9cf7ae1d3046a58efbfd1c289aa40de9
Ro Dixon <rodixondesigns@gmail.com>
2018-10-17 09:34:28 -0400

    I've minified the CSS/HTML
    Minification occured.

--- hire/js/roData.js
+++
@@ -1,5 +0,0 @@
-// grid dummy data
-var roData {
-"About": innerHTML = '<object type="text/html" data="html/About.html" ></object>'
-"Family": innerHTML = '<object type="text/html" data="html/family.html" ></object>'
-}
