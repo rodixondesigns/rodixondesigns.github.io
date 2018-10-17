5222d55964945ad969ccfb0e3ccdffbc59cc5989
Ro Dixon <rodixondesigns@gmail.com>
2018-10-17 09:13:31 -0400

    Adding Professional Portfolio
    Adding Professional Portfolio

---
+++ hire/js/roData.js
@@ -0,0 +1,5 @@
+// grid dummy data
+var roData {
+"About": innerHTML = '<object type="text/html" data="html/About.html" ></object>'
+"Family": innerHTML = '<object type="text/html" data="html/family.html" ></object>'
+}
